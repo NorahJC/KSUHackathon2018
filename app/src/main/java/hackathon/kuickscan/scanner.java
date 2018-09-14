9638666e57e801b4689ae8445aea2ca5d1b16469
[norahjc] <[norahjeanc@gmail.com]>
2018-09-14 17:33:54 -0400

    Added scanner details: scanner intent and scanner contraints; gradle dependencies added for zxing and an aar file

--- app/src/main/java/hackathon/kuickscan/scanner.java
+++ app/src/main/java/hackathon/kuickscan/scanner.java
@@ -4,13 +4,51 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.app.Activity;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class scanner extends AppCompatActivity {
-
+    private Button zxing_barcode_scanner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
+        zxing_barcode_scanner = (Button) findViewById(R.id.zxing_barcode_scanner);
+        final Activity activity = this;
+        zxing_barcode_scanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.PRODUCT_CODE_TYPES);
+                integrator.setPrompt("Scan");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator. setBarcodeImageEnabled(false);
+                integrator.initiateScan();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "You cancelled the scanning", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "Item Added to Cart", Toast.LENGTH_LONG).show();
+            }
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
     }
 
     public void goToCart(View v){
@@ -26,4 +64,6 @@ public class scanner extends AppCompatActivity {
         startActivity(moveToMain);
         finish();
     }
+
+
 }
